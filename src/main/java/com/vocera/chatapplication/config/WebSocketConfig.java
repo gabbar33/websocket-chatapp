772384bdf502ba6a912bd0bdeb7256fdd0478b1a
gabbarsisodiya <gabbar.sisodiya@nagarro.com>
2019-01-30 14:04:48 +0530

    intial commit

---
+++ src/main/java/com/vocera/chatapplication/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.vocera.chatapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+/**
+ * Created by Gabbar Sisodia on 27/01/19.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
